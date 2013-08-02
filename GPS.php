53991b385635ea5debecc95234fa77621db7c3b1
Milan Matejcek <milan.matejcek@gmail.com>
2013-08-02 07:48:48 +0200

    support new gps format

--- GPS.php
+++ GPS.php
@@ -25,18 +25,21 @@ class GPS extends Object {
 
     public function match($gps) {
         $found = array();
-        if (preg_match('~^(\d{1,3}\.\d*?)(N|S), ?(\d{1,3}\.\d*?)(E|W)$~i', $gps, $found)) {
-            //'50.4113628N, 14.9032000E
+        if (preg_match('~^(\d{1,3}\.\d+?)(N|S), ?(\d{1,3}\.\d+?)(E|W)$~i', $gps, $found)) {
+            //50.4113628N, 14.9032000E
             $this->setCoordinate(self::checkCoordinate($found[3], $found[4]), self::checkCoordinate($found[1], $found[2]));
-        } elseif (preg_match('~(-?\d{1,3}\.\d*), ?(-?\d{1,3}\.\d*)$~', $gps, $found)) {
-            //'50.4113628, 14.9032000'
+        } elseif (preg_match('~(-?\d{1,3}\.\d+), ?(-?\d{1,3}\.\d+)$~', $gps, $found)) {
+            //50.4113628, 14.9032000
             $this->setCoordinate($found[2], $found[1]);
-        } elseif (preg_match('~^(N|S) ?(\d{1,3})°(\d{1,2}\.\d*?)\', ?(W|E) ?(\d{1,3})°(\d{1,2}\.\d*?)\'$~i', $gps, $found)) {
+        } elseif (preg_match('~^(N|S) ?(\d{1,3})°(\d{1,2}\.\d+?)\', ?(W|E) ?(\d{1,3})°(\d{1,2}\.\d+?)\'$~i', $gps, $found)) {
             //N 50°24.68177', E 14°54.19200'
             $this->setCoordinate(self::checkCoordinate(self::degToDec($found[5], $found[6]), $found[4]), self::checkCoordinate(self::degToDec($found[2], $found[3]), $found[1]));
-        } elseif (preg_match('~^(\d{1,3})°(\d{1,2})\'(\d{1,2}\.\d*?)"(N|S), ?(\d{1,3})°(\d{1,2})\'(\d{1,2}\.\d*?)"(W|E)$~i', $gps, $found)) {
+        } elseif (preg_match('~^(\d{1,3})°(\d{1,2})\'(\d{1,2}\.\d+?)"(N|S), ?(\d{1,3})°(\d{1,2})\'(\d{1,2}\.\d+?)"(W|E)$~i', $gps, $found)) {
             //50°24'40.906"N, 14°54'11.520"E
             $this->setCoordinate(self::checkCoordinate(self::degToDec($found[5], $found[6], $found[7]), $found[8]), self::checkCoordinate(self::degToDec($found[1], $found[2], $found[3]), $found[4]));
+        } elseif (preg_match('~^(N|S)(\d{1,3}\.\d+?)° ?(E|W)(\d{1,3}\.\d+?)°$~i', $gps, $found)){
+            //N49.20811° E19.04247°
+            $this->setCoordinate(self::checkCoordinate($found[4], $found[3]), self::checkCoordinate($found[2], $found[1]));
         } else {
             throw new GPSException('Unsupported coordinate. ' . $gps);
         }
