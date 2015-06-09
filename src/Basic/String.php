cd703ef7a422d7e6d530766d5ccc0b84a26e4056
Milan Matějček <milan.matejcek@csfd.cz>
2015-06-09 09:27:20 +0200

    Add methods to String class.

--- src/Basic/String.php
+++ src/Basic/String.php
@@ -2,80 +2,107 @@
 
 namespace h4kuna\DataType\Basic;
 
-use h4kuna\DataType\Location\Gps;
-use Iterator;
+use h4kuna\DataType\Location;
 
 /**
- * 
- * 
  * @author Milan Matějček
  */
 final class String
 {
 
-    private function __constructor()
-    {
-        
-    }
+	private function __constructor()
+	{
 
-    /**
-     * 
-     * @param string|int|float $value
-     * @return float
-     */
-    public static function toFloat($value)
-    {
-        return Float::fromString($value);
-    }
+	}
 
-    /**
-     * 
-     * @param int|string $value
-     * @return int
-     */
-    public static function toInt($value)
-    {
-        return Int::fromString($value);
-    }
+	/**
+	 *
+	 * @param string|int|float $value
+	 * @return float
+	 */
+	public static function toFloat($value)
+	{
+		return Float::fromString($value);
+	}
 
-    /**
-     * 
-     * @param string $value
-     * @return float[]
-     */
-    public static function toGps($value)
-    {
-        return Gps::fromString($value);
-    }
+	/**
+	 *
+	 * @param int|string $value
+	 * @return int
+	 */
+	public static function toInt($value)
+	{
+		return Int::fromString($value);
+	}
 
-    /**
-     * 
-     * @param array|Iterator|string $value
-     * @return array
-     */
-    public static function toSet($value)
-    {
-        return Set::fromString($value);
-    }
+	/**
+	 *
+	 * @param string $value
+	 * @return float[]
+	 */
+	public static function toGps($value)
+	{
+		return Location\Gps::fromString($value);
+	}
 
-    /**
-     * Change engoding from 1250 or 8859-2 to UTF-8
-     *
-     * @param string $s
-     * @return string
-     */
-    public static function autoUTF($s)
-    {
-        // detect UTF-8
-        if (preg_match('#[\x80-\x{1FF}\x{2000}-\x{3FFF}]#u', $s))
-            return $s;
+	/**
+	 *
+	 * @param array|\Iterator|string $value
+	 * @return array
+	 */
+	public static function toSet($value)
+	{
+		return Set::fromString($value);
+	}
 
-        // detect WINDOWS-1250
-        if (preg_match('#[\x7F-\x9F\xBC]#', $s))
-            return iconv('WINDOWS-1250', 'UTF-8', $s);
+	/**
+	 * Change engoding from 1250 or 8859-2 to UTF-8
+	 *
+	 * @param string $s
+	 * @return string
+	 */
+	public static function autoUTF($s)
+	{
+		// detect UTF-8
+		if (preg_match('#[\x80-\x{1FF}\x{2000}-\x{3FFF}]#u', $s))
+			return $s;
 
-        // assume ISO-8859-2
-        return iconv('ISO-8859-2', 'UTF-8', $s);
-    }
+		// detect WINDOWS-1250
+		if (preg_match('#[\x7F-\x9F\xBC]#', $s))
+			return iconv('WINDOWS-1250', 'UTF-8', $s);
+
+		// assume ISO-8859-2
+		return iconv('ISO-8859-2', 'UTF-8', $s);
+	}
+
+	/**
+	 * foo_bar => fooBar
+	 * @param string $string
+	 * @return string
+	 */
+	public static function toCamel($string)
+	{
+		return preg_replace_callback('/_([a-z])/', 'camelCallback', $string);
+	}
+
+	/**
+	 * foo_bar => FooBar
+	 * @param string $string
+	 * @return string
+	 */
+	public static function toPascal($string)
+	{
+		return ucfirst(self::toCamel($string));
+	}
+
+	/**
+	 * FooBar => foo_bar
+	 * @param string $string
+	 * @return string
+	 */
+	public static function toUnderscore($string)
+	{
+		return strtolower(preg_replace_callback('/(.)([A-Z][a-z])|([a-z])([A-Z])/', 'underscoreCallback', $string));
+	}
 
 }
--- tests/src/Basic/StringTest.php
+++ tests/src/Basic/StringTest.php
@@ -5,37 +5,73 @@ namespace h4kuna\DataType\Basic;
 class StringTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testToFloat()
-    {
-        $this->assertSame(1.1, String::toFloat('1.1'));
-    }
+	public function testToFloat()
+	{
+		$this->assertSame(1.1, String::toFloat('1.1'));
+	}
 
-    public function testToInt()
-    {
-        $this->assertSame(1, String::toInt('1'));
-    }
+	public function testToInt()
+	{
+		$this->assertSame(1, String::toInt('1'));
+	}
 
-    public function testToGps()
-    {
-        $coordinate = String::toGps('51.1, 14.1');
-        array_walk($coordinate, function(&$v) {
-            $v = (string) round($v, 1);
-        });
-        $this->assertSame(array('51.1', '14.1'), $coordinate);
-    }
+	public function testToGps()
+	{
+		$coordinate = String::toGps('51.1, 14.1');
+		array_walk($coordinate, function(&$v) {
+			$v = (string) round($v, 1);
+		});
+		$this->assertSame(array('51.1', '14.1'), $coordinate);
+	}
 
-    public function testToSet()
-    {
-        $this->assertSame(array('foo' => TRUE, 'bar' => TRUE), String::toSet('foo,bar'));
-    }
+	public function testToSet()
+	{
+		$this->assertSame(array('foo' => TRUE, 'bar' => TRUE), String::toSet('foo,bar'));
+	}
 
-    public function testAutoUTF()
-    {
-        $str = 'Příliš žluťoučký kůň úpěl ďábelské ódy.';
-        $this->assertSame($str, String::autoUTF(iconv('UTF-8', 'ISO-8859-2', $str)));
-        $this->assertSame($str, String::autoUTF(iconv('UTF-8', 'CP1250', $str)));
-        $this->assertSame($str, String::autoUTF(iconv('UTF-8', 'UTF-8', $str)));
-        $this->assertSame($str, String::autoUTF($str));
-    }
+	public function testAutoUTF()
+	{
+		$str = 'Příliš žluťoučký kůň úpěl ďábelské ódy.';
+		$this->assertSame($str, String::autoUTF(iconv('UTF-8', 'ISO-8859-2', $str)));
+		$this->assertSame($str, String::autoUTF(iconv('UTF-8', 'CP1250', $str)));
+		$this->assertSame($str, String::autoUTF(iconv('UTF-8', 'UTF-8', $str)));
+		$this->assertSame($str, String::autoUTF($str));
+	}
+
+	public function testToUnderscore()
+	{
+		$tests = array(
+			'startMIDDLELast' => 'start_middle_last',
+			'simpleXML' => 'simple_xml',
+			'PDFLoad' => 'pdf_load',
+			'simpleTest' => 'simple_test',
+			'easy' => 'easy',
+			'HTML' => 'html',
+			'AString' => 'a_string',
+			'Some4Numbers234' => 'some4_numbers234',
+			'TEST123String' => 'test123_string',
+		);
+		foreach ($tests as $value => $expeted) {
+			$this->assertSame($expeted, String::toUnderscore($value));
+		}
+	}
+
+	public function testToCamel()
+	{
+		$tests = array(
+			'start_middle_last' => 'startMiddleLast',
+			'simple_xml' => 'simpleXml',
+			'pdf_load' => 'pdfLoad',
+			'simple_test' => 'simpleTest',
+			'easy' => 'easy',
+			'html' => 'html',
+			'a_string' => 'aString',
+			'some4_numbers234' => 'some4Numbers234',
+			'test123_string' => 'test123String',
+		);
+		foreach ($tests as $value => $expeted) {
+			$this->assertSame($expeted, String::toCamel($value));
+		}
+	}
 
 }
