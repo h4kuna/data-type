a88acc1f077db64b3843d0b7ff12608ac529ba01
Milan Matějček <milan.matejcek@csfd.cz>
2015-06-17 09:48:21 +0200

    Typo Number.

--- src/Number/Math.php
+++ src/Number/Math.php
@@ -2,7 +2,7 @@
 
 namespace h4kuna\DataType\Number;
 
-use h4kuna\DataType\LogicalException;
+use h4kuna\DataType;
 
 /**
  * @author Milan Matějček
@@ -10,85 +10,79 @@ use h4kuna\DataType\LogicalException;
 final class Math
 {
 
-    private function __construct()
-    {
-        
-    }
+	private function __construct() {}
 
-    /**
-     * Allow number in interval and correct it.
-     * 
-     * @param $number
-     * @param $max float
-     * @param $min float
-     * @return float
-     * @throws LogicException
-     */
-    public static function interval($number, $max, $min = 0)
-    {
-        if ($max < $min) {
-            throw new LogicalException('Maximum is less than minimum.');
-        }
-        return max($min, min($max, $number));
-    }
+	/**
+	 * Allow number in interval and correct it.
+	 * @param $number
+	 * @param $max float
+	 * @param $min float
+	 * @return float
+	 * @throws DataType\InvalidArgumentsException
+	 */
+	public static function interval($number, $max, $min = 0)
+	{
+		if ($max < $min) {
+			throw new DataType\InvalidArgumentsException('Maximum is less than minimum.');
+		}
+		return max($min, min($max, $number));
+	}
 
-    /**
-     * Round method to zero point five.
-     * @example 1.24 -> 1.0, 1.25 -> 1.5, 1.74 -> 1.5, 1.75 -> 2.0
-     * 
-     * @param float $num
-     * @return float
-     */
-    public static function round5($num)
-    {
-        if ($num < 0) {
-            $floor = ceil($num);
-            $i = -1;
-        } else {
-            $floor = floor($num);
-            $i = 1;
-        }
-        $decimal = abs($num - $floor);
-        if ($decimal < 0.25) {
-            return $floor;
-        }
+	/**
+	 * Round method to zero point five.
+	 * @example 1.24 -> 1.0, 1.25 -> 1.5, 1.74 -> 1.5, 1.75 -> 2.0
+	 *
+	 * @param float $num
+	 * @return float
+	 */
+	public static function round5($num)
+	{
+		if ($num < 0) {
+			$floor = ceil($num);
+			$i = -1;
+		} else {
+			$floor = floor($num);
+			$i = 1;
+		}
+		$decimal = abs($num - $floor);
+		if ($decimal < 0.25) {
+			return $floor;
+		}
 
-        if (0.25 <= $decimal && $decimal < 0.75) {
-            return $floor + (0.5 * $i);
-        }
-        return $floor + (1 * $i);
-    }
+		if (0.25 <= $decimal && $decimal < 0.75) {
+			return $floor + (0.5 * $i);
+		}
+		return $floor + (1 * $i);
+	}
 
-    /**
-     * Safe division.
-     * 
-     * @param float $up
-     * @param float $down
-     * @return type
-     */
-    public static function safeDivision($up, $down)
-    {
-        if (!$down) {
-            return NULL;
-        }
-        return $up / $down;
-    }
+	/**
+	 * Safe division.
+	 * @param float $up
+	 * @param float $down
+	 * @return float|int|NULL
+	 */
+	public static function safeDivision($up, $down)
+	{
+		if (!$down) {
+			return NULL;
+		}
+		return $up / $down;
+	}
 
-    /**
-     * Factorial.
-     * 
-     * @param int $n
-     * @return int
-     */
-    public static function factorial($n)
-    {
-        if ($n == 0) {
-            return 1;
-        }
-        if ($n < 0) {
-            throw new LogicalException('The number cann\'t negative number.');
-        }
-        return $n * self::factorial($n - 1);
-    }
+	/**
+	 * Factorial.
+	 * @param int $n
+	 * @return int
+	 */
+	public static function factorial($n)
+	{
+		if ($n == 0) {
+			return 1;
+		}
+		if ($n < 0) {
+			throw new LogicalException('The number cann\'t negative number.');
+		}
+		return $n * self::factorial($n - 1);
+	}
 
 }
--- src/Number/RomeNumber.php
+++ src/Number/RomeNumber.php
@@ -12,57 +12,55 @@ namespace h4kuna\DataType\Number;
 class RomeNumber extends NonObject
 {
 
-    static private $numbers = array(
-        "M" => 1000, "CM" => 900,
-        "D" => 500, "CD" => 400,
-        "C" => 100, "XC" => 90,
-        "L" => 50, "XL" => 40,
-        "X" => 10, "IX" => 9,
-        "V" => 5, "IV" => 4,
-        "I" => 1
-    );
+	static private $numbers = array(
+		"M" => 1000, "CM" => 900,
+		"D" => 500, "CD" => 400,
+		"C" => 100, "XC" => 90,
+		"L" => 50, "XL" => 40,
+		"X" => 10, "IX" => 9,
+		"V" => 5, "IV" => 4,
+		"I" => 1
+	);
 
-    /**
-     * Prevede cislo na arabske cislo
-     *
-     * @param int $number
-     * @return string
-     */
-    public static function getRome($number)
-    {
-        $return = NULL;
-        foreach (self::$numbers as $key => $val) {
-            $return .= str_repeat($key, floor($number / $val));
-            $number %= $val;
-        }
-        return $return;
-    }
+	/**
+	 * Prevede cislo na arabske cislo
+	 * @param int $number
+	 * @return string
+	 */
+	public static function getRome($number)
+	{
+		$return = NULL;
+		foreach (self::$numbers as $key => $val) {
+			$return .= str_repeat($key, floor($number / $val));
+			$number %= $val;
+		}
+		return $return;
+	}
 
-    /**
-     * Prevede arabske cislo na rimnske
-     *
-     * @param string $rome
-     * @return int
-     */
-    public static function getArabic($rome)
-    {
-        $return = 0;
-        $move = FALSE;
-        $rome = str_split(strtoupper((string) $rome));
-        foreach ($rome as $key => $val) {
-            if ($move === TRUE) {
-                $move = FALSE;
-                continue;
-            }
+	/**
+	 * Prevede arabske cislo na rimnske
+	 * @param string $rome
+	 * @return int
+	 */
+	public static function getArabic($rome)
+	{
+		$return = 0;
+		$move = FALSE;
+		$rome = str_split(strtoupper((string) $rome));
+		foreach ($rome as $key => $val) {
+			if ($move === TRUE) {
+				$move = FALSE;
+				continue;
+			}
 
-            if (isset($rome[$key + 1]) && isset(self::$numbers[$val . $rome[$key + 1]])) {
-                $return += self::$numbers[$val . $rome[$key + 1]];
-                $move = TRUE;
-            } else {
-                $return += self::$numbers[$val];
-            }
-        }
-        return $return;
-    }
+			if (isset($rome[$key + 1]) && isset(self::$numbers[$val . $rome[$key + 1]])) {
+				$return += self::$numbers[$val . $rome[$key + 1]];
+				$move = TRUE;
+			} else {
+				$return += self::$numbers[$val];
+			}
+		}
+		return $return;
+	}
 
 }
