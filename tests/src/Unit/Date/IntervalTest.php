989a929dc02b16a5b676dd6c3cad895ce1a780ff
Milan Matějček <milan.matejcek@gmail.com>
2024-04-22 07:40:27 +0200

    feat(Interval): add Interval count seconds

---
+++ tests/src/Unit/Date/IntervalTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace h4kuna\DataType\Tests\Unit\Date;
+
+require __DIR__ . '/../../../bootstrap.php';
+
+use Closure;
+use DateInterval;
+use DateTime;
+use h4kuna\DataType\Date\Interval;
+use Tester\Assert;
+use Tester\TestCase;
+
+final class IntervalTest extends TestCase
+{
+	/**
+	 * @return array<string|int, array{0: Closure(static):void}>
+	 */
+	public static function dataToSeconds(): array
+	{
+		return [
+			'29. february exists' => [
+				static function (self $self) {
+					$interval = (new DateTime('2024-06-01 12:13:14'))->diff(new DateTime('2023-06-01 00:00:00'));
+					$self->assertToSeconds(31666394, $interval);
+				},
+			],
+			'only 28. february' => [
+				static function (self $self) {
+					$interval = (new DateTime('2023-06-01 12:13:14'))->diff(new DateTime('2022-06-01 00:00:00'));
+					$self->assertToSeconds(31579994, $interval);
+				},
+			],
+		];
+	}
+
+
+	/**
+	 * @param Closure(static):void $assert
+	 * @dataProvider dataToSeconds
+	 */
+	public function testToSeconds(Closure $assert): void
+	{
+		$assert($this);
+	}
+
+
+	public function assertToSeconds(
+		int $expected,
+		DateInterval $dateInterval,
+	): void
+	{
+		Assert::same($expected, Interval::toSeconds($dateInterval));
+	}
+}
+
+(new IntervalTest())->run();
